067fb36fbb70a6c046a2003ebabf9fd155af6b8c
Janvi Patel <janvi.patel482@gmail.com>
2018-12-11 23:27:02 +0000

    Added pseudo code and Dedupe

--- flask_app.py
+++ flask_app.py
@@ -9,8 +9,27 @@ def main():
 @app.route('/process_inputs', methods=['POST'])
 def process_inputs():
     name = request.form.get('input_name', '')
+    # verify that the name has at least 4 characters
+    # verify that the first letter of the name is uppercase
+
     dropdown = request.form.get('input_dropdown', '')
+    # if the dropdown value is red fish, show a red fish picture
+    # if it is instead blue fish, then show a blue fish picture
+
     select = request.form.get('input_select', '')
     freeform = request.form.get('input_freeform', '')
     return render_template("main_page.html", input_data=dropdown,
                            output="You're a wizard %s." % name)
+
+def dedupe(x):
+    y = []
+    for i in x:
+        if i not in x:
+            y.append(i)
+    return y
+
+animals = ['cats', 'cats', 'cats', 'dogs', 'pandas']
+print(dedupe(animals)) # prints ['cats', 'dogs', 'pandas']
+
+
+
